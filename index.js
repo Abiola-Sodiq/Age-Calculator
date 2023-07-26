90716f0ad3799b7f75eb044a70129c7c1e115d7c
Abiola Sodiq <rasababe01@gmail.com>
2023-07-26 21:27:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let today = new Date();
+let dobInput = document.getElementById("dob");
+const ageDisplay = document.getElementById("result");
+let eligibility = document.getElementById("eligibility")
+let outputAge = '';
+
+dobInput.addEventListener("change", function() {
+    let dateOfBirth = dobInput.value;
+    let dob =dateOfBirth.split("-");
+    let year = dob.filter((item)=>{return item.length >=4
+    });
+    console.log(year);
+    let age = today.getFullYear() - year;
+    outputAge = age
+});
+function showAge() {
+    ageDisplay.innerHTML = outputAge
+    
+    if (outputAge <18) { 
+        eligibility.style.backgroundColor = "red"
+        eligibility.innerHTML= "Oops!! You are not Eligible for this Offer..."
+        
+    } 
+    else if (outputAge >25 ){
+        eligibility.style.backgroundColor = "red"
+        eligibility.innerHTML= "Oops!! You are not Eligible for this Offer..."
+    }
+    else {
+        eligibility.style.backgroundColor = "green"
+
+        eligibility.innerHTML= "Hurray!!! You Qualify for this Offer"
+    }
+}
